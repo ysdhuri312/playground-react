107fd0b405a7b49ceee43309c03d6d684744a6a1
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-11-10 15:26:11 +0530

    minor changes

--- Projects/src/components/Counter2.jsx
+++ Projects/src/components/Counter2.jsx
@@ -18,42 +18,62 @@ const Counter2 = () => {
   const reducer = (state, action) => {
     switch (action.type) {
       case 'INCREAMENT':
-        return state + 1;
+        return { count: state.count + action.payload };
 
       case 'DECREAMENT':
-        return state - 1;
+        return { count: state.count + action.payload };
+
+      case 'INCREAMENTBY10':
+        return { count: state.count + action.payload };
+
+      case 'DECREAMENTBY10':
+        return { count: state.count - action.payload };
 
       case 'RESET':
-        return 0;
+        return { count: state.count - action.payload };
 
       default:
         return state;
     }
   };
 
-  const initialState = 0;
+  const initialState = { count: 0 };
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
       <h1>Counter with useReduce hook</h1>
-      <p>{state}</p>
+      <p>{state.count}</p>
       <button
         onClick={() => {
-          dispatch({ type: 'INCREAMENT' });
+          dispatch({ type: 'INCREAMENT', payload: 1 });
         }}
       >
         Increament
       </button>
       <button
         onClick={() => {
-          dispatch({ type: 'DECREAMENT' });
+          dispatch({ type: 'DECREAMENT', payload: 1 });
         }}
       >
         Decreament
       </button>
       <button
         onClick={() => {
-          dispatch({ type: 'RESET' });
+          dispatch({ type: 'INCREAMENTBY10', payload: 10 });
+        }}
+      >
+        Increament By 10
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: 'DECREAMENTBY10', payload: 10 });
+        }}
+      >
+        Decreament By 10
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: 'RESET', payload: 0 });
         }}
       >
         Reset
